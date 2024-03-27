33280936e01ec85a77094dcdb835b8fcdf22d80f
AFLAH <aflah.cade@gmail.com>
2024-03-27 18:09:37 +0300

    Added a class for the Users table

--- models.py
+++ models.py
@@ -1,1 +1,16 @@
+from config import db, bcrypt
+from datetime import datetime
 
+
+# Create a class for the Users table
+class Users(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(60), unique=True)
+    user_name = db.Column(db.String(100), nullable=False)
+    user_email = db.Column(db.String(100), nullable=False, unique=True)
+    user_password = db.Column(db.String(100), nullable=False)
+    admin = db.Column(db.Boolean, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.user_password, password)
