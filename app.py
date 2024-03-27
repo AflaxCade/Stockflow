f1fa4a4e2ffcee9c914333a7fa77922c09507c57
AFLAH <aflah.cade@gmail.com>
2024-03-28 00:32:05 +0300

    Added customers api endpoints code

--- app.py
+++ app.py
@@ -172,3 +172,101 @@ def delete_user(current_user, public_id):
     db.session.commit()
 
     return jsonify({'message': 'User deleted successfully'}), 200
+
+
+# This route will return a customer by their ID
+@app.route("/customer/<customer_id>", methods=["GET"])
+@token_required
+def get_customer(current_user, customer_id):
+    customer = Customers.query.filter_by(customer_id=customer_id).first()
+    if not customer:
+        return jsonify({"message": "Customer not found"}), 404
+    customer_data = {}
+    customer_data["customer_id"] = customer.customer_id
+    customer_data["customer_name"] = customer.customer_name
+    customer_data["customer_email"] = customer.customer_email
+    customer_data["customer_phone"] = customer.customer_phone
+    customer_data["customer_address"] = customer.customer_address
+    customer_data["date"] = customer.date
+    return jsonify(customer_data), 200
+
+
+# This route will return all customers
+@app.route("/customers", methods=["GET"])
+@token_required
+def get_all_customers(current_user):
+    customers = Customers.query.all()
+    output = []
+    for customer in customers:
+        customer_data = {}
+        customer_data["customer_id"] = customer.customer_id
+        customer_data["customer_name"] = customer.customer_name
+        customer_data["customer_email"] = customer.customer_email
+        customer_data["customer_phone"] = customer.customer_phone
+        customer_data["customer_address"] = customer.customer_address
+        customer_data["date"] = customer.date
+        output.append(customer_data)
+    return jsonify(output), 200
+
+
+# This route will create a new customer
+@app.route("/customers", methods=["POST"])
+@token_required
+def create_customer(current_user):
+    data = request.get_json()
+    customer_name=data["customer_name"]
+    customer_email=data["customer_email"]
+    customer_phone=data["customer_phone"]
+    customer_address=data["customer_address"]
+
+    if not customer_name or not customer_email or not customer_phone or not customer_address:
+        return jsonify({"message": "You must include a name, email, phone and address"}), 400
+
+    existing_customer = Customers.query.filter_by(customer_email=customer_email).first()
+    if existing_customer:
+        return jsonify({"message": "A customer with this email already exists"}), 400
+    
+    new_customer = Customers(
+        customer_name=customer_name,
+        customer_email=customer_email,
+        customer_phone=customer_phone,
+        customer_address=customer_address)
+    db.session.add(new_customer)
+    db.session.commit()
+    return jsonify({"message": "New customer created"}), 201
+
+
+# This route will update a customer
+@app.route("/customer/<customer_id>", methods=["PUT"])
+@token_required
+def update_customer(current_user, customer_id):
+    customer = Customers.query.filter_by(customer_id=customer_id).first()
+    if not customer:
+        return jsonify({"message": "Customer not found"}), 404
+    data = request.get_json()
+    if "customer_name" in data:
+        customer.customer_name = data["customer_name"]
+    if "customer_email" in data:
+        customer.customer_email = data["customer_email"]
+    if "customer_phone" in data:
+        customer.customer_phone = data["customer_phone"]
+    if "customer_address" in data:
+        customer.customer_address = data["customer_address"]
+    if not customer.customer_name or not customer.customer_email or not customer.customer_phone or not customer.customer_address:
+        return jsonify({"message": "Missing required value"}), 400
+    db.session.commit()
+    return jsonify({"message": "Customer updated"}), 200
+
+
+# This route will delete a customer
+@app.route("/customer/<customer_id>", methods=["DELETE"])
+@token_required
+def delete_customer(current_user, customer_id):
+    customer = Customers.query.filter_by(customer_id=customer_id).first()
+    if not customer:
+        return jsonify({"message": "Customer not found"}), 404
+    db.session.delete(customer)
+    db.session.commit()
+    return jsonify({"message": "Customer deleted"}), 200
+
+
